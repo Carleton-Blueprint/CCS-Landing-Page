b86ace63c50611cdaa3dc8ab59da20ecad46481a
HasithDeAlwis <hasithde24@gmail.com>
2024-03-21 22:37:25 -0400

    modified gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,10 @@ module.exports = {
       "accessToken": "owxbf1uvibLAnDGa_9NTPM0lqt-Wiqa3hA5RyeTpN4A",
       "spaceId": "ktj6j9l1ry7f"
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+  }, 
+  "gatsby-plugin-image", 
+  "gatsby-plugin-sharp", 
+  "gatsby-transformer-sharp", 
+  "gatsby-plugin-postcss"
+  ]
 };
