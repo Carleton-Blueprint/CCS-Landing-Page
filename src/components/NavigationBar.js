4c3618002ede3e7ce4f3f55049fae0fbedf9a873
Juno9170 <ryanzhuturtle@gmail.com>
2024-03-26 21:42:18 -0400

    Basic Navigation Bar Divs
    Added basic navigation bar text, no styles because waiting for design to come. Just added it so that I can add styles based on the path of the current page.

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const NavigationBar = ({location}) => {
+  const currentpath = location.pathname;
+  const isCurrentPath = (page) =>{
+    if (currentpath.startsWith(page)){
+      return true;
+    }
+    return false;
+  }
+  
+  return (
+    <div className=''>
+      <div>{currentpath}</div>
+      <div className='container flex gap-10'>
+        <div className={`${currentpath === '/' ? 'font-bold' : 'font-normal'}`}>Home</div>
+        <div className={`${isCurrentPath('/about') ? 'font-bold' : 'font-normal'}`}>About</div>
+        <div className={`${isCurrentPath('/events') ? 'font-bold' : 'font-normal'}`}>Events</div>
+        <div className={`${isCurrentPath('/getInvolved') ? 'font-bold' : 'font-normal'}`}>Get Involved</div>
+        <div className={`${isCurrentPath('/FAQ') ? 'font-bold' : 'font-normal'}`}>FAQ</div>
+        <div className={`${isCurrentPath('/meetTheTeam') ? 'font-bold' : 'font-normal'}`}>Meet the Team</div>
+        <div className={`${isCurrentPath('/contact') ? 'font-bold' : 'font-normal'}`}>Contact</div>
+      </div>
+    </div>
+  )
+}
+
+export default NavigationBar 
