1e4f275aeac7ce81637892ab0fb9968253fe764d
HasithDeAlwis <hasithde24@gmail.com>
2024-03-30 13:20:51 -0400

    altered form styling

---
+++ src/components/contact-us/Form.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const Form = () => {
+  return (
+    <div className='container'>
+        <form className="flex flex-col" name="contact">
+            <label>
+                Name: 
+                <input type='text' name='name' id='name' style={{border: '1px solid black'}} /> {/*temp styling added to see form*/}
+            </label>
+            <label>
+                Phone Number: 
+                <input type='text' name='phoneNumber' id='phoneNumber' style={{border: '1px solid black'}} />
+            </label>
+            <label>
+                Email: 
+                <input type='email' name='email' id='email' style={{border: '1px solid black'}} />
+            </label>
+            <label>
+                Message: 
+                <textarea name='message' id='message' rows="5" style={{border: '1px solid black'}}/>
+            </label>
+            <button type="submit" style={{border: '1px solid black'}}>Send Message</button>
+        </form>
+    </div>
+  )
+}
+
+export default Form
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import NavigationBar from '../components/NavigationBar'
+import NavigationBar from '../components/base/NavigationBar'
+import Form from '../components/contact-us/Form'
 const HomePage = () => {
   return (
-    <div>HELLOO   <NavigationBar /></div>
+    <div>
+      <NavigationBar pathname={'/'}/>
+      <Form />
+    </div>
   )
 }
 
