e37f196a3ac4e93226a365c49c2e8f2eca7493d9
methirah <methira.19@gmail.com>
2024-05-01 17:47:46 -0400

    gradient navbar with some additional styling

--- src/components/base/NavigationBar.js
+++ src/components/base/NavigationBar.js
@@ -10,15 +10,18 @@ const NavigationBar = (props) => {
   }
   
   return (
-    <div className='container'>
-      <div className='container flex gap-10'>
-        <div className={`${currentpath === '/' ? 'font-bold' : 'font-normal'}`}>Home</div>
-        <div className={`${isCurrentPath('/about') ? 'font-bold' : 'font-normal'}`}>About</div>
-        <div className={`${isCurrentPath('/events') ? 'font-bold' : 'font-normal'}`}>Events</div>
+    // change height based on screen
+    <div className='container h-16'>
+      {/* Add custom styles to tailwind config to match figma */}
+      <div className='flex w-screen justify-end items-center bg-gradient-to-r from-black from-23% to-red-700 gap-20  text-white font-poppins'>
+        <div className={`${currentpath === '/' ? 'font-bold' : 'font-normal'}`}>HOME</div>
+        <div className={`${isCurrentPath('/about') ? 'font-bold' : 'font-normal'}`}>ABOUT US</div>
+        <div className={`${isCurrentPath('/events') ? 'font-bold' : 'font-normal'}`}>EVENTS</div>
         <div className={`${isCurrentPath('/getInvolved') ? 'font-bold' : 'font-normal'}`}>Get Involved</div>
         <div className={`${isCurrentPath('/FAQ') ? 'font-bold' : 'font-normal'}`}>FAQ</div>
         <div className={`${isCurrentPath('/meetTheTeam') ? 'font-bold' : 'font-normal'}`}>Meet the Team</div>
-        <div className={`${isCurrentPath('/contact') ? 'font-bold' : 'font-normal'}`}>Contact</div>
+        <div className={`${isCurrentPath('/contact') ? 'font-bold' : 'font-normal'}`}>CONTACT</div>
+        <div></div>
       </div>
     </div>
   )
