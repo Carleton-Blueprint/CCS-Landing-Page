7b032af66c16a3f9a15686a7b5020f2b1a537191
Juno9170 <ryanzhuturtle@gmail.com>
2024-05-06 00:26:09 -0400

    Made Gallery Component A lot more readable

--- src/components/about-us-components/gallery-components/AboutUsGallery.js
+++ src/components/about-us-components/gallery-components/AboutUsGallery.js
@@ -7,8 +7,9 @@ const AboutUsGallery = (props) => {
   const movingLeft = useRef(false);
   const imagesLength = allImages.length;
   const startIndex = useRef(0);
-  const animationTimeInt = useRef(350);
-  const animationTimeString = useRef("duration-[350ms]")
+  const animationTimeInt = useRef(200);
+  const animationTimeString = useRef("duration-[200ms]")
+  const animationTypeString = useRef("ease-in-out")
   const isRunning = useRef(false)
 	const [visibleImages, setVisibleImages] = useState([0,1,2,3,4])
 
@@ -24,49 +25,53 @@ const AboutUsGallery = (props) => {
   const increment = (n, isInitial) => {
     if (n <= 0) {
       isRunning.current = false;
+      animationTypeString.current = 'ease-in-out';
       return;
     };
-    if (isInitial){
-      let start = (startIndex.current + 1) % imagesLength;
+  
+    // Set the animation type to ease in out if its just a one move over. set to linear if the move is more than one.
+    animationTypeString.current = (n === 1 && isInitial) ? 'ease-in-out' : 'ease-linear';
+  
+    const updateImages = () => {
+      const start = (startIndex.current + 1) % imagesLength;
       startIndex.current = start;
-      setVisibleImages(generateVisibleImages(startIndex.current,false))
-      increment(n - 1 ,false);
+      setVisibleImages(generateVisibleImages(startIndex.current, false));
+      increment(n - 1, false);
+    };
+  
+    // Execute updateImages immediately if isInitial, else with a delay
+    if (isInitial) {
+      updateImages();
+    } else {
+      setTimeout(updateImages, animationTimeInt.current);
     }
-    else{
-      setTimeout(() => {
-        let start = (startIndex.current + 1) % imagesLength;
-        startIndex.current = start;
-        setVisibleImages(generateVisibleImages(startIndex.current,false))
-        increment(n - 1, false);
-      },animationTimeInt.current);
-    }
-    
   };
+  
   const decrement = (n, isInitial) => {
     if (n <= 0) {
       isRunning.current = false;
+      animationTypeString.current = 'ease-in-out';
       return;
-    };
-
-    if (isInitial){
-      let start = (startIndex.current - 1);
-      if (start === -1) {start = imagesLength-1}    
-      startIndex.current = start
-      
-      setVisibleImages(generateVisibleImages(startIndex.current,true))
-      decrement(n - 1 ,false);
     }
-    else{
-      setTimeout(() => {
-        let start = (startIndex.current - 1);
-        if (start === -1) {start = imagesLength-1}    
-        startIndex.current = start
-        
-        setVisibleImages(generateVisibleImages(startIndex.current,true))
-        decrement(n - 1 ,false);
-      },animationTimeInt.current);
+    // Set the animation type to ease in out if its just a one move over. set to linear if the move is more than one.
+    animationTypeString.current = (n === 1 && isInitial) ? 'ease-in-out' : 'ease-linear';
+
+    const updateImages = () => {
+      let start = startIndex.current - 1;
+      if (start === -1) start = imagesLength - 1; // Wrap around if index goes below 0
+      startIndex.current = start;
+      setVisibleImages(generateVisibleImages(startIndex.current, true));
+      decrement(n - 1, false);
+    };
+  
+    // Execute updateImages immediately if isInitial, else with a delay
+    if (isInitial) {
+      updateImages();
+    } else {
+      setTimeout(updateImages, animationTimeInt.current);
     }
   };
+  
 
   const handleImageNav = (distance, direction) => {
       if (distance === -1 || isRunning.current) {return;}
@@ -90,51 +95,64 @@ const AboutUsGallery = (props) => {
     const brightnessArray = ['brightness-50', 'brightness-75','brightness-100','brightness-75','brightness-50']
     return brightnessArray[index]
   }
-  const getCss = (imageIndex) =>{
-    //Set left to keep state based on index greater than or less than -1 or 0 index of the visible images list
-    if (!visibleImages.includes(imageIndex)){
-      return movingLeft.current ? `right-0 opacity-0 transition-all linear ${animationTimeString.current} w-0 h-0` : `left-0 opacity-0 transition-all linear ${animationTimeString.current} w-0 h-0`
+  const getCss = (imageIndex) => {
+    // Early return for images not currently visible
+    if (!visibleImages.includes(imageIndex)) {
+      return `${movingLeft.current ? 'right-0' : 'left-0'} opacity-0 transition-all ${animationTypeString.current} ${animationTimeString.current} w-0 h-0`;
     }
-    if (visibleImages[0] === imageIndex && movingLeft.current){
-      return `left-0 opacity-100 transition-opacity linear ${animationTimeString.current} w-60 h-48 z-20`
+  
+    // Get default styles for visible images
+    const baseStyle = `opacity-100 ${animationTypeString.current} ${animationTimeString.current}`;
+    const zIndexAndSize = {
+      0: `z-20 w-60 h-48`,
+      1: `z-30 w-80 h-64`,
+      2: `z-40 w-[364px] h-80`,
+      3: `z-30 w-80 h-64`,
+      4: `z-20 w-60 h-48`
+    };
+  
+    // Specific positioning logic
+    let positionStyle = '';
+    switch (visibleImages.indexOf(imageIndex)) {
+      case 0:
+        positionStyle = `left-0 ${movingLeft.current ? 'transition-opacity' : 'transition-all'}`;
+        break;
+      case 1:
+        positionStyle = 'left-1/4 transform -translate-x-1/2 transition-all';
+        break;
+      case 2:
+        positionStyle = 'left-1/2 transform -translate-x-1/2 transition-all';
+        break;
+      case 3:
+        positionStyle = 'left-3/4 transform -translate-x-1/2 transition-all';
+        break;
+      case 4:
+        positionStyle = `right-0 ${movingLeft.current ? 'transition-all' : 'transition-opacity'}`;
+        break;
+      default:
+        break;
     }
-    if (visibleImages[0] === imageIndex && !movingLeft.current){
-      return `left-0 opacity-100 transition-all linear ${animationTimeString.current} w-60 h-48 z-20`
-    }
-    if (visibleImages[1] === imageIndex){
-      return `left-1/4 opacity-100 transform -translate-x-1/2 transition-all linear ${animationTimeString.current} z-30 w-80 h-64`
-    }
-    if (visibleImages[2] === imageIndex){
-      return `left-1/2 transform -translate-x-1/2 opacity-100 transition-all linear ${animationTimeString.current} z-40 w-[364px] h-80`
-    }
-    if (visibleImages[3] === imageIndex){
-      return `left-3/4 transform -translate-x-1/2 opacity-100 transition-all linear ${animationTimeString.current} z-30 w-80 h-64`
-    }
-    if (visibleImages[4] === imageIndex  && !movingLeft.current){
-      return `right-0 opacity-100 transition-opacity linear ${animationTimeString.current} z-20 w-60 h-48`
-    }
-    if (visibleImages[4] === imageIndex  && movingLeft.current){
-      return `right-0 opacity-100 transition-all linear ${animationTimeString.current} z-20 w-60 h-48`
-    }
-
-  }
+  
+    return `${positionStyle} ${baseStyle} ${zIndexAndSize[visibleImages.indexOf(imageIndex)]}`;
+  };
+  
 
   const setAnimationTime = (skipDistance) =>{
     let duration, durationString;
 
     switch (skipDistance) {
       case 1:
-        duration = 350;
-        durationString = 'duration-[350ms]';
+        duration = 200;
+        durationString = 'duration-[200ms]';
         break;
       case 2:
       case 3:
-        duration = 150;
-        durationString = 'duration-[150ms]';
+        duration = 100;
+        durationString = 'duration-[100ms]';
         break;
       default:
-        duration = 80;
-        durationString = 'duration-[80ms]';
+        duration = 50;
+        durationString = 'duration-[50ms]';
         break;
     }
     animationTimeString.current = durationString
@@ -148,7 +166,7 @@ const AboutUsGallery = (props) => {
         className='bg-[#ABAAAA] w-[40px] h-[40px] transition-all ease-in-out duration-300 rounded-full'
         onMouseEnter={(e) => e.currentTarget.className = 'bg-[#e91c24] w-[40px] h-[40px] -translate-x-1 transition-all ease-in-out duration-300 rounded-full'}
         onMouseLeave={(e) => e.currentTarget.className = 'bg-[#ABAAAA] w-[40px] h-[40px] translate-x-0 transition-all ease-in-out duration-300 rounded-full'}
-        onClick={() => decrement(1, true, true)}
+        onClick={() => {setAnimationTime(1); decrement(1, true, true)}}
         onMouseDown={(e) => e.preventDefault()}
         >
           <svg viewBox='0 0 40 40' xmlns="http://www.w3.org/2000/svg">
@@ -173,12 +191,12 @@ const AboutUsGallery = (props) => {
         </div>
 
 
-        
+
         <div
         className='bg-[#ABAAAA] w-[40px] h-[40px] transition-all ease-in-out duration-300 rounded-full'
         onMouseEnter={(e) => e.currentTarget.className = 'bg-[#e91c24] w-[40px] h-[40px] translate-x-1 transition-all ease-in-out duration-300 rounded-full'}
         onMouseLeave={(e) => e.currentTarget.className = 'bg-[#ABAAAA] w-[40px] h-[40px] translate-x-0 transition-all ease-in-out duration-300 rounded-full'}
-        onClick={() => increment(1, true, true)}
+        onClick={() => {setAnimationTime(1); increment(1, true, true)}}
         onMouseDown={(e) => e.preventDefault()}
         >
           <svg viewBox='0 0 40 40' xmlns="http://www.w3.org/2000/svg">
