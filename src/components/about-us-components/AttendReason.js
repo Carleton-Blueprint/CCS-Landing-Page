1a76060786c15ec8c8e67caf01709fa61d208b9b
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-05-10 00:16:50 -0400

    finished why attend reason - no animation

--- src/components/about-us-components/AttendReason.js
+++ src/components/about-us-components/AttendReason.js
@@ -4,8 +4,9 @@ import decadron from "../../images/decadron.png"
 const AttendReason =  (props) => {
     console.log(props)
     return (
-        <div className="bg-cover bg-center h-64 w-64 flex items-center justify-center" style={{ backgroundImage: `url(${decadron})` }}>
-            <p className="text-center p-8 text-white">{props.title}<br/><br/>{props.subtitle}</p>
+        <div className="bg-cover bg-center h-64 w-64 items-center justify-center" style={{ backgroundImage: `url(${decadron})` }}>
+            <p className="text-center pt-10 text-white"><strong>{props.title}</strong></p>
+            <p className="text-center px-4 py-4 pt-4 text-white">{props.subtitle}</p>
         </div>
     )
 }
--- src/components/about-us-components/WhyAttend.js
+++ src/components/about-us-components/WhyAttend.js
@@ -3,8 +3,7 @@ import AttendReason from "./AttendReason";
 
 const WhyAttend = (props) => {
     return (
-        <div>
-            <h2>WhyAttend</h2>
+        <div className="flex items-center">
             {props.reasons.map((r) => {
                 return (<AttendReason key={r.id} title = {r.title} subtitle={r.subtitle}/>)
             })}
