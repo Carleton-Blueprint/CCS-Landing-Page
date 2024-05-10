0dd2b1d4c6ec8b5c758c3965cd705a4daea1fff7
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-05-09 22:02:35 -0400

    made decadrons

--- src/components/about-us-components/JoinReason.js
+++ src/components/about-us-components/JoinReason.js
@@ -1,8 +1,28 @@
 import React from "react"
+import decadron from "../../images/decadron.png"
+// import {Stage, Layer, RegularPolygon, Text} from "react-konva"
+// import Konva from 'konva'
 
 const JoinReason =  (props) => {
+    
+    /*const width = 200
+    const half = width/2
+    const sixth = width/6
+    const twelfth = width/12
+    let points = half+","+0+" "+0+","+half+" "+sixth+","+(width+twelfth)+" "+(width-sixth)+","+(width+twelfth)+" "+width+","+half
+    <svg width="400" height="400">
+        <polygon points={points}></polygon>
+        <text fill='red' width={200} x="200" y="100" textAnchor="middle" alignmentBaseline="middle" lengthAdjust="spacingAndGlyphs">{props.reason}</text>
+    </svg>*/
+
+    //  const decadron = {
+    //     backgroundImage: '../../images/decadron.png'
+    //  }
+
     return (
-        <h1>Reason To Join: {props.reason}</h1>
+        <div className="bg-cover bg-center h-64 w-64 flex items-center justify-center" style={{ backgroundImage: `url(${decadron})` }}>
+            <p className="text-center p-4 text-white">{props.reason}</p>
+        </div>
     )
 }
 
