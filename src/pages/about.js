14a350a71448876da355271fafbbd2087ae1e6b7
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-05-14 00:53:40 -0400

    statistics from contenful shown with gradient background on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import NavigationBar from '../components/base/NavigationBar'
-import { Seo } from '../components/base/Seo'
-const About = () => {
-  return (
-    <div><NavigationBar pathname={'/about'}/></div>
-  )
-}
+import React from "react";
+import NavigationBar from "../components/base/NavigationBar";
+import { Seo } from "../components/base/Seo";
+import AboutUsStatistics from "../components/about-us-components/AboutUsStatistics";
+import { graphql } from "gatsby";
 
-export default About
+const About = ({data}) => {
+  return (
+    <div>
+      <NavigationBar pathname={"/about"} />
+      <AboutUsStatistics stats={data.allContentfulStatistic.nodes}/>
+    </div>
+  );
+};
+
+export default About;
 
 export const Head = () => (
-  <Seo title = "About" description="This is the about page"/>
-)
+  <Seo title="About" description="This is the about page" />
+);
+
+export const query = graphql`
+query MyQuery {
+  allContentfulStatistic {
+    nodes {
+      bottomSubtitle
+      subtitle1
+      statistic
+      id
+    }
+  }
+}
+`;
