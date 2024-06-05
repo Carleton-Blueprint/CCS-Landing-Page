a0ca772ea48b3dbf675d107d2d25839a26aa7662
Juno9170 <ryanzhuturtle@gmail.com>
2024-06-04 20:16:55 -0400

    Finished home gallery

---
+++ src/components/home-components/home-gallery-components/HomeGalleryDots.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const HomeGalleryDots = (props) => {
+    const translateIncrement = props.increment
+    const mapDots = Array(props.amount).fill(null);
+    const activeImage = props.activeImage
+    const handleImageNav = (translateX) => props.callbackChangeImage(translateX)
+
+    const r = props.size === 'lg' ? 8.5 : props.size === 'sm' ? 5.5 : 7
+    const cxy =  props.size === 'lg' ? 9 : props.size === 'sm' ? 6 : 7.5
+
+    const handleDotClick = (index) =>{
+        console.log(index)
+        handleImageNav(translateIncrement*index)
+    }
+    return (
+        <div className='flex gap-3 pt-8 w-fit'>
+        {mapDots.map((_, index) => (
+            <div key={index}>
+                <svg width={cxy*2} height={cxy*2} viewBox={`0 0 ${cxy*2} ${cxy*2}`} fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle 
+                cx={cxy} cy={cxy} r={r} fill={`${activeImage === index ? '#08679D' : '#D9D9D9'}`}
+                className=' transition-all ease-in-out duration-200'
+                onMouseEnter={(e) => e.target.setAttribute('fill', '#0292B7')}
+                onMouseLeave={(e) => activeImage === index ? e.target.setAttribute('fill', '#08679D') : e.target.setAttribute('fill', '#D9D9D9') }
+                onClick={() => handleDotClick(index)}
+                />
+                </svg>
+
+            </div>
+            ))}
+        </div>
+    )
+}
+
+export default HomeGalleryDots
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,38 @@ import NavigationBar from '../components/base/NavigationBar'
 import Form from '../components/contact-us-components/Form'
 import { Seo } from '../components/base/Seo'
 import { graphql } from "gatsby"
-
-
+import HomeImageCarousel from '../components/home-components/home-gallery-components/HomeImageCarousel'
 const HomePage = ({data}) => {
+
   return (
-    <div>
+    <div className='bg-red-200 h-screen'>
       <NavigationBar pathname={'/'}/>
       <Form />
-
+      <HomeImageCarousel images={data.allContentfulAboutUsGallery.nodes} size = 'lg'/>
     </div>
   )
 }
 
+export const query = graphql`
+query{
+  allContentfulAboutUsGallery {
+    nodes {
+      galleryImage {
+        gatsbyImageData(
+          layout: CONSTRAINED
+          placeholder: BLURRED
+          width: 600
+          formats: [AUTO, WEBP, AVIF]
+        )
+      }
+    }
+  }
+}
+`;
 export default HomePage
 // query to return all galleryImages from the AboutUsGallery
 
 export const Head = () => (
   <Seo title = "Home Page"  description="This is the home page"/>
 )
+
