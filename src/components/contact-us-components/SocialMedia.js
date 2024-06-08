a177daad7cdce24970196da25d749ad72f4522e9
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-06-08 00:59:52 -0400

    add links to social media

--- src/components/contact-us-components/SocialMedia.js
+++ src/components/contact-us-components/SocialMedia.js
@@ -9,18 +9,19 @@ const SocialMedia = () => {
     <div className="flex-col justify-center items-center">
       <h1 className="text-center font-bold text-xl text-gray-600 m-[10px]">Get in touch with CUSEC</h1>
       <div className="flex justify-center items-center">
-        <div className="bg-gray-300 rounded-full w-[40px] p-[10px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
+        
+        <a href="mailto:info@cusec.net" target="_blank" className="bg-gray-300 rounded-full w-[40px] p-[10px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
           <img src={mail}/>
-        </div>
-        <div className="bg-gray-300 rounded-full w-[40px] p-[10px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
+        </a>
+        <a href="https://x.com/cusec" target="_blank" className="bg-gray-300 rounded-full w-[40px] p-[10px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
           <img src={twitter}/>
-        </div>
-        <div className="bg-gray-300 rounded-full w-[40px] p-[10px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
+        </a>
+        <a href="https://www.linkedin.com/company/cusec/mycompany/" target="_blank"  className="bg-gray-300 rounded-full w-[40px] p-[10px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
           <img src={linkedin}/>
-        </div>
-        <div className="bg-gray-300 rounded-full w-[40px] p-[6px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
+        </a>
+        <a href="https://discord.gg/AzDrgHAnWf" target="_blank"  className="bg-gray-300 rounded-full w-[40px] p-[6px] m-[5px]" style={{'boxShadow': 'inset 0 3px 5px 1px rgb(0,0,0,0.3)'}}>
           <img src={discord}/>
-        </div>
+        </a>
 
       </div>
     </div>
