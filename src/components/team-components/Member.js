25766a3954a27ad247517793398fbf55871bc01f
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-06-14 23:20:44 -0400

    remove email link for member

--- src/components/team-components/Member.js
+++ src/components/team-components/Member.js
@@ -19,10 +19,8 @@ const Member = (props) => {
     return(
         <div className="bg-cover w-[200px] h-[300px] rounded-tr-3xl rounded-bl-3xl " style={headshotStyle}>
             <div onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} className="transition-all duration-6000 ease-linear top-0 left-0  w-[200px] h-[300px] rounded-tr-3xl rounded-bl-3xl" style={overlayStyle}>
-                <div className={isHovering ? "flex relative top-[80%] left-[45%]" : "flex relative top-[80%] left-[45%] hidden"}>
-                    <a href={"mailto:"+props.member.email} target="_blank" rel="noreferrer" className="border-2 border-black rounded-full w-[40px] p-[7px] m-[5px] h-[40px] hover:bg-red-300">
-                        <img src={mailIcon} alt="email"/>
-                    </a>
+                
+                <div className={isHovering ? "flex relative top-[82%] left-[70%]" : "flex relative top-[80%] left-[45%] hidden"}>
                     <a href={props.member.linkedin} target="_blank" rel="noreferrer" className="border-2 border-black rounded-full w-[40px] p-[7px] m-[5px] h-[40px] hover:bg-red-300">
                         <img src={linkedinIcon} alt="linkedin"/>
                     </a>
