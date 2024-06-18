edd563e534f7bcea1b6d03a121feaa03188c1515
Diandra Inturire <danainturire@cmail.carleton.ca>
2024-06-17 22:19:51 -0400

    starting logos

--- src/components/base/Footer.js
+++ src/components/base/Footer.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import ig from '../../images/igicon.png'
+import email from '../../images/emailicon.png'
+import linkedin  from '../../images/linkedinicon.png'
+import phone from '../../images/phoneicon.png'
+import logo1 from '../../images/carletonlogo.png'
+import logo2 from '../../images/cuseclogo.png'
 
-const Footer = () => {
+
+const Footer = (props) => {
   return (
-    <div>Footer</div>
+    <div className=' flex flex-col'>
+    <div className= "bg-[url('../images/Footerbackground.png')] w-full h-80">
+      <div className= 'flex justify-center'>
+        <div className='flex justify-center'>
+        <div>
+          <img src ={ig} alt="instagram"></img>
+        </div>
+        <div>
+          <img src ={email} alt="mail"></img>
+        </div>
+        <div>
+          <img src ={linkedin} alt ="linkedin"></img>
+        </div>
+        <div>
+          <img src ={phone} alt="call"></img>
+        </div> 
+        </div>
+        <div className ='flex justify-center items-center'>
+        <div>
+          <img src ={logo1} alt="carletonlogo"></img>
+        </div>
+        <div>
+          <img src ={logo2} alt="cuseclogo"></img>
+        </div>
+        </div>
+        
+     
+      </div>
+     
+    </div>
+    </div>
+
   )
 }
 
---
+++ src/components/base/FooterElements.js
@@ -0,0 +1,25 @@
+
+// export const FooterContainer = styled.div `
+// `
+// export const FooterBody = styled.div `
+// `
+// export const FooterIcons = styled.div `
+// `
+// export const FooterLinks = styled.div `
+// `
+// export const FooterLogos = styled.div `
+// `
+// export const FooterLogo = styled.div `
+// `
+// export const FooterSocialMedia = styled.div `
+// `
+// export const FooterSocialMediaIcons = styled.div `
+// `
+// export const FooterNavbar = styled.div `
+// `
+// export const FooterNavbarHeader = styled.div `
+// `
+
+
+
+
