43b45171f4f7c6d20307cfb0ea279562ee5009c6
HasithDeAlwis <hasithde24@gmail.com>
2024-06-24 10:59:24 -0400

    Added header component plus cleanup

---
+++ src/components/base/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div className="">
+      <div className="flex absolute mt-[10%] pl-[40%]">
+        <h1 className="text-6xl font-bold text-center text-white">{props.title}</h1>
+      </div>
+      <img
+        className="object-contain w-screen"
+        src={props.background}
+        alt="Contact Us Background"
+      />
+    </div>
+  );
+};
+
+export default Header;
