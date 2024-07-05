d8a975f13c0d86f5f6ff47b9b101c89458916043
methirah <methira.19@gmail.com>
2024-07-05 17:17:55 -0400

    base styling done

--- src/components/faq/Accordian.js
+++ src/components/faq/Accordian.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
-import Question from './Question'
+import React from 'react';
+import Question from './Question';
 
 const Accordian = (props) => {
 
-
-    return (
+  return (
     <div className="container flex justify-center">
-      <div className='flex flex-col items-center rounded-3xl bg-nero'>
-          {props.data.allContentfulFaq.nodes.map((faq, index) => {
-          return <div
-                  key={index} 
-                  className="">
-                    <Question faq={faq}/>
-                </div>
+      <div className='flex flex-col items-center rounded-3xl bg-nero relative'>
+        {props.data.allContentfulFaq.nodes.map((faq, index) => {
+          return (
+            <div
+              key={index}
+              className={`w-full ${index !== 0 ? '-mt-10' : ''} relative z-${10*index}`}>
+              <Question faq={faq} index={index} isLast={index === props.data.allContentfulFaq.nodes.length - 1}/>
+            </div>
+          );
         })}
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Accordian
+export default Accordian;
--- src/components/faq/Question.js
+++ src/components/faq/Question.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
 
 function Question(props) {
   const [isOpen, setIsOpen] = useState(false);
   const question = props.faq.faqQuestion;
   const answer = JSON.parse(props.faq.faqAnswer.raw);
-  
+  const index = props.index;
+  const isLast = props.isLast;
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   }
+
   return (
-    // figure out overlap effect, add proper icons, integrate rich text renderer, add transitions?
     <div className="flex justify-center">
-      <div className={`border-t rounded-t-3xl border-gray-400 w-[40rem] text-white font-poppins ${isOpen ? 'bg-gradient-to-b from-nero to-red-stop ' : ''}`}>
+      <div className={`border-t rounded-t-3xl rounded-b-3xl z-${index*10} ${isLast ? '' : 'pb-8'} border-gray-400 w-[40rem] text-white font-poppins ${isOpen ? 'bg-gradient-to-b from-nero to-red-stop ' : 'bg-nero'}`}>
         <button 
-          className="w-full flex justify-between items-center pt-4 pb-4 px-4 "
+          className="w-full flex justify-between items-center pt-4 pb-4 px-4"
           onClick={toggleOpen}
         >
           <span className='mx-2'>{question}</span>
