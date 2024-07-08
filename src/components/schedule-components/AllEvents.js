330d4c4a7ad8910dd928482f26dbe6d59d4a3987
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-07-08 09:55:50 -0400

    schedule header

--- src/components/schedule-components/AllEvents.js
+++ src/components/schedule-components/AllEvents.js
@@ -9,7 +9,7 @@ const AllEvents = ({events}) => {
             0px 10vh 500px #E91C24`
     }
     return (
-        <div className="grid place-items-center" style={{'backgroundColor': '#41151B'}}>
+        <div className="grid place-items-center mt-[-5%]" style={{'backgroundColor': '#41151B'}}>
             <div className=" bg-[#111] w-[700px] h-screen" style={shadow}>
                 {events.map(element => {
                     return (
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -19,9 +19,7 @@ const Schedule = ({data}) => {
     const dayNumber = date
     return (
         <>
-        {/*flex justify-center items-align sticky top-0 p-8 w-screen bg-[#232323]  */}
-          <img src={greyBackground} className="absolute w-screen object-fit h-[170px]"></img>
-        <div className="flex justify-center items-align sticky top-0 p-8 w-screen">
+        <div className="flex justify-center items-align sticky top-0 p-8 w-screen bg-bottom" style={{'background-image': `url(${greyBackground})`, 'background-size': '100% 100%'}}>
             <button onClick={() => setDate(date-1)} className= {dayNumber > 1 ? "absolute left-20 rounded-full p-2" : "absolute left-20 rounded-full p-2 hidden"} style={arrowShadow}>
                 <svg xmlns="http://www.w3.org/2000/svg"
                 fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="size-8"
