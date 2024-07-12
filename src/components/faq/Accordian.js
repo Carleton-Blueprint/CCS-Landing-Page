2c1035fa2c5c9b17773e0505b7f7f5f6c64f935f
methirah <methira.19@gmail.com>
2024-07-12 17:51:19 -0400

    font, icons, and some more responsive design added

--- src/components/faq/Accordian.js
+++ src/components/faq/Accordian.js
@@ -5,12 +5,12 @@ const Accordian = (props) => {
 
   return (
     <div className="container flex justify-center">
-      <div className='flex flex-col items-center rounded-3xl bg-nero relative'>
+      <div className='flex flex-col items-center rounded-[2rem] md:w-[36rem] bg-nero relative'>
         {props.data.allContentfulFaq.nodes.map((faq, index) => {
           return (
             <div
               key={index}
-              className={`w-full ${index !== 0 ? '-mt-10' : ''} relative z-${10*index}`}>
+              className={`w-[20rem] md:w-[36rem] ${index !== 0 ? '-mt-10' : ''} relative z-${10*index}`}>
               <Question faq={faq} index={index} isLast={index === props.data.allContentfulFaq.nodes.length - 1}/>
             </div>
           );
--- src/components/faq/Question.js
+++ src/components/faq/Question.js
@@ -11,20 +11,31 @@ function Question(props) {
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   }
+  console.log(answer);
 
   return (
     <div className="flex justify-center">
-      <div className={`border-t rounded-t-3xl rounded-b-3xl z-${index*10} ${isLast ? '' : 'pb-8'} border-gray-400 w-[40rem] text-white font-poppins ${isOpen ? 'bg-gradient-to-b from-nero to-red-stop ' : 'bg-nero'}`}>
+      <div className={`border-t-[1.5px] rounded-t-[2rem] rounded-b-[2rem] z-${index*10} ${isLast ? '' : 'pb-8'} border-slate-600 w-[20rem] md:w-full text-white  font-poppins ${isOpen ? 'bg-gradient-to-b from-nero to-red-stop ' : 'bg-nero'}`}>
         <button 
           className="w-full flex justify-between items-center pt-4 pb-4 px-4"
           onClick={toggleOpen}
         >
-          <span className='mx-2'>{question}</span>
-          <span className="toggle-icon">{isOpen ? '▲' : '▼'}</span>
+          <span className='mx-2 md:mx-4 my-2 font-semibold text-xs md:text-sm'>{question}</span>
+          <span className="mr-2">
+            {isOpen 
+              ? 
+              <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7"/>
+              </svg> 
+              : 
+              <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
+              </svg>}
+          </span>
         </button>
 
         {isOpen && (
-          <div className="text-sm px-4 pb-6">
+          <div className="text-xs md:text-sm font-extralight md:font-light mx-6 md:mx-8 pb-6">
             {answer.content.map((content, index) => (
               <p key={index}>
                 {content.content.map((subcontent, subIndex) => (
