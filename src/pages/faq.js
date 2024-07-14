dab6406c6869e5389aa6acfcf4e313141ad96db4
HasithDeAlwis <hasithde24@gmail.com>
2024-07-14 18:54:56 -0400

    bug fix

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,26 +1,23 @@
-import { graphql } from 'gatsby'
-import React from 'react'
-import NavigationBar from '../components/base/NavigationBar'
-import Accordian from '../components/faq/Accordian'
-import Header from '../components/base/Header'
-import background from '../images/faq-header.svg'
+import { graphql } from 'gatsby';
+import React from 'react';
+import NavigationBar from '../components/base/NavigationBar';
+import Accordian from '../components/faq-components/Accordian';
+import Header from '../components/base/Header';
+import background from '../images/faq-header.svg';
 
 function Faq({ data }) {
-
-
   return (
     <>
-      <NavigationBar pathname={'/FAQ'}/>
-      <Header title='FAQ' background={background}/>
-      <div className='container'>
-        <Accordian  data={data} />
+      <NavigationBar pathname={'/FAQ'} />
+      <Header title="FAQ" background={background} />
+      <div className="container">
+        <Accordian data={data} />
       </div>
     </>
-  )
+  );
 }
 
-export const query = graphql
-`
+export const query = graphql`
   query {
     allContentfulFaq {
       nodes {
@@ -31,6 +28,6 @@ export const query = graphql
       }
     }
   }
-`
+`;
 
-export default Faq
+export default Faq;
