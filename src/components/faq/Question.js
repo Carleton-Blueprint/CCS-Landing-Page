c060d9a2f49b73dcd51ac84dfefcadc942c7b28c
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-07-22 23:28:06 -0400

    accordian animation

--- src/components/faq/Question.js
+++ src/components/faq/Question.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import RichText from './FaqRichTextRenderer';
 
 function Question(props) {
@@ -12,8 +12,12 @@ function Question(props) {
     setIsOpen(!isOpen);
   }
   return (
-    <div className="flex justify-center">
-      <div className={`border-t-[1.5px] rounded-t-[2rem] rounded-b-[2rem] z-${index*10} ${isLast ? '' : 'pb-8'} border-slate-600 w-[20rem] w-full text-white  font-poppins ${isOpen ? 'bg-gradient-to-b from-nero to-red-stop ' : 'bg-nero'}`}>
+    <div className={`border-t-[1.5px] rounded-t-[2rem] pb-8 ${isLast ? 'rounded-b-[2rem]' : ''} justify-center ${isOpen ? 'bg-gradient-to-b from-nero to-red-stop' : 'bg-nero'}`}>
+    <motion.div
+        initial={false}
+        onClick={toggleOpen}
+        className={`border-t-[1.5px] rounded-t-[2rem]  z-${index*10} ${isLast ? '' : 'pb-8'} border-slate-600 w-[20rem] w-full text-white  font-poppins`}
+      >
         <button 
           className="flex items-center justify-between w-full px-4 pt-4 pb-4"
           onClick={toggleOpen}
@@ -31,26 +35,27 @@ function Question(props) {
               </svg>}
           </span>
         </button>
+      </motion.div>
 
+      <AnimatePresence initial={false}>
         {isOpen && (
-          <div className="pb-6 mx-6 text-xs md:text-sm font-extralight md:font-light md:mx-8">
-            <RichText content={answer.content} />
-            {/* {answer.content.map((content, index) => (
-              <p key={index}>
-                {content.content.map((subcontent, subIndex) => (
-                  <span key={subIndex}>
-                    {subcontent.nodeType === "hyperlink" ? (
-                      <a href={subcontent.data.uri} className="underline">{subcontent.content[0].value}</a>
-                    ) : (
-                      <span>{subcontent.value}</span>
-                    )}
-                  </span>
-                ))}
-              </p>
-            ))} */}
-          </div>
+          <motion.section
+            // Add animations for the accordion content
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.5, ease: [0.04, 0.62, 0.23, 0.98] }}
+          >
+            <div className='text-white text-xs md:text-sm font-extralight md:font-light md:mx-8'>
+              <RichText content={answer.content} />
+            </div>
+          </motion.section>
         )}
-      </div>
+      </AnimatePresence>
     </div>
   );
 }
