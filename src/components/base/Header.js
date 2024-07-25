91366888607aad62682b754ed6c23a503f6caf11
HasithDeAlwis <hasithde24@gmail.com>
2024-07-24 20:41:54 -0400

    adjusted header size on smaller screen

--- src/components/base/Header.js
+++ src/components/base/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Header = (props) => {
   return (
@@ -7,9 +7,11 @@ const Header = (props) => {
         className="object-cover object-bottom lg:h-[80vh] md:h-[60vh] sm:h-[50vh] xl:h-[90vh] w-screen z-0"
         src={props.background}
         alt="Header Background"
-        />
+      />
       <div className="flex absolute justify-center z-10 top-[12vh] sm:top-[12vh] lg:top-[15vh] top-[8vh] w-screen text-center">
-        <h1 className="lg:text-[70px] md:text-[50px] sm:text-[40px] text-[30px] font-bold text-white">{props.title}</h1>
+        <h1 className="lg:text-[70px] md:text-[50px] sm:text-[40px] text-[40px] font-bold text-white mt-4">
+          {props.title}
+        </h1>
       </div>
     </div>
   );
