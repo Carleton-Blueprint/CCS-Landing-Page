f71a09742d2c696761842ef3407fcc664cba2fdb
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-07-29 22:05:54 -0400

    dropdown on hover not on click

--- src/components/base/NavigationBar.js
+++ src/components/base/NavigationBar.js
@@ -117,6 +117,18 @@ const NavigationBar = (props) => {
                     }`}
                   >
                     <a href="/about-us">ABOUT US</a>
+                      <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className=" ml-2 inline size-6"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
                   </li>
                   <li
                     className={`my-16 ${
@@ -188,7 +200,8 @@ const NavigationBar = (props) => {
           >
             <motion.div
               initial={false}
-              onClick={() => setAboutHover((prev) => !prev)}
+              onHoverStart={() => setAboutHover((prev) => true)}
+              onHoverEnd={() => setAboutHover((prev) => false)}
             >
               <div>
                 ABOUT US
