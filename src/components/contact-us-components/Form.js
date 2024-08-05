2e0e4c9faa5ba45def8e0739127811bb8c322dbe
HasithDeAlwis <hasithde24@gmail.com>
2024-08-05 12:05:16 -0400

    bug: removed deleted file

--- src/components/contact-us-components/Form.js
+++ src/components/contact-us-components/Form.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Toast from '../base/Toast';
 
 const Form = () => {
   const [formData, setFormData] = useState({
