a84f9fe0825f573cc208991f3a85a65ef4bdb2be
HasithDeAlwis <hasithde24@gmail.com>
2024-08-06 08:18:04 -0400

    improvement: animations for get-involved page

--- src/components/get-involved-components/GetInvolvedCard.js
+++ src/components/get-involved-components/GetInvolvedCard.js
@@ -1,34 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const GetInvolvedCard = ({data}) => {
-    const title = data.title;
-    const description = data.description.description;
-    const icon = data.icon.file.url
-    const altTag = data.icon.description;
-    const buttonText = data.linkTitle;
-    const link = data.link
-    
-  return  (
-    <div className='relative flex flex-col w-10/12 text-white rounded-tr-[3rem] rounded-bl-[3rem] bg-primaryGray h-[16rem] sm:w-[32rem] sm:h-[22rem]'>
-        <div className='px-6 pt-8'>
-            <p className='mb-4 text-lg font-semibold sm:text-2xl font-poppins'>{title}</p>
-            <p className='text-xs sm:text-lg font-extralight font-poppins'>{description}</p>
+const GetInvolvedCard = ({ data }) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const title = data?.title;
+  const description = data?.description.description;
+  const icon = data?.icon.file.url;
+  const altTag = data?.icon.description;
+  const buttonText = data?.linkTitle;
+  const link = data?.link;
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      className="relative flex flex-col w-10/12 text-white transition-all duration-300
+                rounded-tr-[3rem] rounded-bl-[3rem] 
+                hover:rounded-tr-none hover:rounded-bl-none hover:rounded-tl-[3rem] hover:rounded-br-[3rem] 
+                bg-primaryGray h-[16rem] sm:w-[32rem] sm:h-[22rem] shadow-red-bottom-left hover:shadow-red-bottom-left-hover"
+    >
+      <div className="px-6 pt-8">
+        <p className="mb-4 text-lg font-semibold sm:text-2xl font-poppins">
+          {title}
+        </p>
+        <p className="text-xs sm:text-lg font-extralight font-poppins">
+          {description}
+        </p>
+      </div>
+      <div className="flex justify-between w-full">
+        <div className="p-6">
+          <a href={link}>
+            <button className="absolute w-1/3 border border-white rounded-3xl bottom-8 bg-primaryGray">
+              <p className="p-2 text-xs font-bold sm:text-lg">{buttonText}</p>
+            </button>
+          </a>
         </div>
-        <div className='flex justify-between w-full'>
-            <div className='p-6'>
-                <a href={link}>
-                    <button  className='absolute w-1/3 border border-white rounded-3xl bottom-8 bg-primaryGray'>
-                        <p className='p-2 text-xs font-bold sm:text-lg'>{buttonText}</p>
-                    </button>
-                </a> 
-            </div>
-            <div className='absolute bottom-0 right-0 w-1/2 sm:w-[16rem]'>
-                <img src={icon} alt={altTag}/>
-            </div>
+        <div
+          className={`absolute bottom-0 right-0 w-1/2 sm:w-[16rem]
+            ${isHovering ? 'animate-growShrink' : ''}`}
+        >
+          <img src={icon} alt={altTag} />
         </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default GetInvolvedCard
-
+export default GetInvolvedCard;
