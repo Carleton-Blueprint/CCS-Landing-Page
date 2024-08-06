e58a7ed40e8a42b4e96d4c7706dedc2d0103e6c7
HasithDeAlwis <hasithde24@gmail.com>
2024-08-06 08:34:47 -0400

    improvement: hover animations for get-involved buttons

--- src/components/get-involved-components/GetInvolvedCard.js
+++ src/components/get-involved-components/GetInvolvedCard.js
@@ -13,12 +13,12 @@ const GetInvolvedCard = ({ data }) => {
     <div
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
-      className="relative flex flex-col w-10/12 text-white transition-all duration-300
+      className="relative flex flex-col w-10/12 transition-all duration-30 text-white 
                 rounded-tr-[3rem] rounded-bl-[3rem] 
                 hover:rounded-tr-none hover:rounded-bl-none hover:rounded-tl-[3rem] hover:rounded-br-[3rem] 
                 bg-primaryGray h-[16rem] sm:w-[32rem] sm:h-[22rem] shadow-red-bottom-left hover:shadow-red-bottom-left-hover"
     >
-      <div className="px-6 pt-8">
+      <div className="px-6 pt-8 ">
         <p className="mb-4 text-lg font-semibold sm:text-2xl font-poppins">
           {title}
         </p>
@@ -29,8 +29,10 @@ const GetInvolvedCard = ({ data }) => {
       <div className="flex justify-between w-full">
         <div className="p-6">
           <a href={link}>
-            <button className="absolute w-1/3 border border-white rounded-3xl bottom-8 bg-primaryGray">
-              <p className="p-2 text-xs font-bold sm:text-lg">{buttonText}</p>
+            <button className="absolute w-1/3 transition-all duration-200 ease-in border border-white active:border-[#676666] rounded-3xl bottom-8 bg-primaryGray hover:bg-darkGrey active:bg-[#676666]">
+              <p className="p-2 text-xs font-bold sm:text-lg hover:text-[#151515] active:text-white">
+                {buttonText}
+              </p>
             </button>
           </a>
         </div>
