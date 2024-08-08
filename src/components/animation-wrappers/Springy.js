4b4092a64a75a15f440525e7d311e83743f5ee6d
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-08-07 21:32:28 -0400

    springy component
    there is a prop hover that if true only enablea the springy effect if the component is being hovered.

--- src/components/animation-wrappers/Springy.js
+++ src/components/animation-wrappers/Springy.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
-export default Springy(({children, speed, magnitude}) => {
+const Springy = ({ children, speed = '5', magnitude = '5', hover = false}) => {
+  const [isHovering, setIsHovering] = useState(true);
   return (
     <motion.div
+      className='transition-transform'
+      onHoverStart={(e) => {
+        if (hover) {
+          setIsHovering(true);
+        }
+      }}
+      onHoverEnd={(e) => {
+        if (hover) {
+          setIsHovering(false);
+        }
+      }}
       animate={{
-        y: ['0%', '10%', '0%', '-10%', '0%'],
+        y: isHovering ? [0, `${magnitude}%`, 0] : [0],
       }}
       transition={{
-        duration: 4,
+        duration: speed,
         ease: 'easeInOut',
         repeat: Infinity,
         repeatType: 'loop',
+        times: [0, 0.5, 1],
       }}
     >
       {children}
     </motion.div>
   );
-});
+};
+
+export default Springy;
