a2c4bcfa6e0ab1b14d79226e39dd08613b4bc0b1
methirah <methira.19@gmail.com>
2024-08-08 09:11:59 -0400

    added threshold height for navbar fadeout and fixed hamburger bug

--- src/components/base/Layout.js
+++ src/components/base/Layout.js
@@ -8,31 +8,37 @@ const Layout = ({ pathname, backgroundColour, children }) => {
   const timeoutRef = useRef(null); // Reference to store the timeout
 
   useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
 
+    const handleScroll = () => {
+      const viewportHeight = window.innerHeight;
+      const currentScrollY = window.scrollY;
+      const threshold = viewportHeight * 0.22;
       // Clear any existing timeout to reset the timer
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
       }
 
-      if (currentScrollY > scrollY.current) {
+      if (currentScrollY > scrollY.current && currentScrollY > threshold) {
         // Scrolling down
         setIsVisible(false);
-      } else {
+      } else if (currentScrollY < scrollY.current) {
         // Scrolling up
         setIsVisible(true);
       }
 
       // Set a new timeout to hide the navigation bar after 3 seconds of inactivity
-      timeoutRef.current = setTimeout(() => {
-        setIsVisible(false);
-      }, 2000);
-
+      if (currentScrollY > threshold) {
+        timeoutRef.current = setTimeout(() => {
+          setIsVisible(false);
+        }, 2000);
+      }
       scrollY.current = currentScrollY;
     };
 
     const handleMouseMove = (event) => {
+      const viewportHeight = window.innerHeight;
+      const currentScrollY = window.scrollY;
+      const threshold = viewportHeight * 0.21;
       // Clear any existing timeout to reset the timer
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
@@ -41,7 +47,7 @@ const Layout = ({ pathname, backgroundColour, children }) => {
       if (event.clientY < 150) {
         // Cursor is within 150 pixels of the top of the viewport
         setIsVisible(true);
-      } else {
+      } else if (currentScrollY > threshold) {
         // Set a new timeout to hide the navigation bar after 2 seconds if cursor is not near the top
         timeoutRef.current = setTimeout(() => {
           setIsVisible(false);
