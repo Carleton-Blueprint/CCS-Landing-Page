74525b76a8a1085d7f6bfb20059d0493cd9d708d
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-08-09 17:41:59 -0400

    Springy hover prop removed

--- src/components/animation-wrappers/Springy.js
+++ src/components/animation-wrappers/Springy.js
@@ -10,24 +10,12 @@ import { useState } from 'react';
 //the whileHover feature in framer-motion was not used because once hover ends the component
 //stays in the position it was when the hover ended and does not revert to its original position
 
-const Springy = ({ children, speed = '5', magnitude = '5', hover = false }) => {
-  const [isHovering, setIsHovering] = useState(!hover);
+const Springy = ({ children, speed = '5', magnitude = '5'}) => {
   const getAnimation = (magnitude) => {
-    return { y: isHovering ? [0, `${magnitude}%`, 0] : [0] };
+    return { y:  [0, `${magnitude}%`, 0] };
   };
   return (
     <motion.div
-      className="transition-transform"
-      onHoverStart={(e) => {
-        if (hover) {
-          setIsHovering(true);
-        }
-      }}
-      onHoverEnd={(e) => {
-        if (hover) {
-          setIsHovering(false);
-        }
-      }}
       animate={getAnimation(magnitude)}
       transition={{
         duration: speed,
