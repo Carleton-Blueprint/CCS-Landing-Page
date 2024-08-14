fb3f31ce09bc7dcc49d4a4476cfc8e4aa07185d9
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-08-14 00:08:16 -0400

    fixed site menu (footer) issue on about us page

--- src/components/about-us-components/AboutUsStatistics.js
+++ src/components/about-us-components/AboutUsStatistics.js
@@ -5,7 +5,7 @@ const AboutUsStatistics = (props) => {
   return (
     <div className="w-full">
       <div className="flex justify-center w-full py-20 overflow-hidden bg-gradient-to-br from-red-600 to-black">
-        <div className="w-full h-full absolute transform -translate-y-[80px] bg-gradient-to-b from-white to-30% z-10"></div>
+        <div className="h-[50%] w-full absolute transform -translate-y-[80px] bg-gradient-to-b from-white to-30% z-10"></div>
         <div
           className="w-[92%] lg:w-fit h-fit bg-white bg-opacity-75 
         rounded-[80px] p-5 z-50 transition-transform hover:animate-growShrink"
