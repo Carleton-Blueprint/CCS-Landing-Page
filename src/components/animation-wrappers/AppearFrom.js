174fd0bd720e4a69156bc6666edb65cd9b32470b
HasithDeAlwis <hasithde24@gmail.com>
2024-08-14 08:49:05 -0400

    ui(header):optimized header dropdown

--- src/components/animation-wrappers/AppearFrom.js
+++ src/components/animation-wrappers/AppearFrom.js
@@ -1,30 +1,30 @@
-import React from "react";
-import {motion} from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 
-const AppearFrom = ({children, direction="left", speed="2"}) => {
+const AppearFrom = ({ children, direction = 'left', speed = '2' }) => {
+  const getAnimation = (direction) => {
+    switch (direction) {
+      case 'left':
+        return { x: ['-100vw', 0] };
+      case 'right':
+        return { x: ['100vw', 0] };
+      case 'top':
+        return { y: ['-100vh', 0] };
+      case 'bottom':
+        return { y: ['100vh', 0] };
+      default:
+        return { x: ['-100vw', 0] };
+    }
+  };
+  return (
+    <motion.div
+      animate={getAnimation(direction)}
+      transition={{ duration: speed }}
+      style={{ willChange: 'transform' }}
+    >
+      {children}
+    </motion.div>
+  );
+};
 
-    const getAnimation = (direction) => {
-        switch (direction){
-            case 'left':
-                return {x: ['-100vw', 0]}
-            case 'right':
-                return {x: ['100vw', 0]}
-            case 'top':
-               return {y: ['-100vh', 0]}
-            case 'bottom':
-                return {y: ['100vh', 0]}
-            default:
-               return {x: ['-100vw', 0]}
-        } 
-    } 
-    return (
-        <motion.div
-            animate={getAnimation(direction)}
-            transition={{ duration: speed }}
-        >
-            {children}
-        </motion.div>
-    )
-}
-
-export default AppearFrom
+export default AppearFrom;
